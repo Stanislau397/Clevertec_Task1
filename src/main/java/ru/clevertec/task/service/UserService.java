23cf96162f201220f61a27f6bf7c079b4cd07593
Stanislau Kachan <Lancer397@gmail.com>
2023-02-05 23:08:39 +0300

    docs: add documentation to register method

--- src/main/java/ru/clevertec/task/service/UserService.java
+++ src/main/java/ru/clevertec/task/service/UserService.java
@@ -6,5 +6,11 @@ import ru.clevertec.task.entity.User;
 @Service
 public interface UserService {
 
+    /**
+     * 
+     * @param user
+     * @param password
+     * @return
+     */
     User register(User user, String password);
 }

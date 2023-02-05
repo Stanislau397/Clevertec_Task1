9ff91318d87850bdcc041a505b5f0fcc0d7833e6
Stanislau Kachan <Lancer397@gmail.com>
2023-02-05 22:31:21 +0300

    feat: allow user to registrate in application

--- src/main/java/ru/clevertec/task/service/UserService.java
+++ src/main/java/ru/clevertec/task/service/UserService.java
@@ -1,7 +1,10 @@
 package ru.clevertec.task.service;
 
 import org.springframework.stereotype.Service;
+import ru.clevertec.task.entity.User;
 
 @Service
 public interface UserService {
+
+    User registerUser(User user, String password);
 }
